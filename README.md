02ab60441179cf0da7048855336817c140329157
zhouzhengrui <zhouzhengrui@gmail.com>
2016-09-07 23:44:19 +0800

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+# 所用插件
+
+- [导航 - mmenu](http://mmenu.frebsite.nl/)
+- [弹出框 - fancyBox](http://fancyapps.com/fancybox/)
+- [触摸滑动 (tab/slideshow) - swiper](http://www.swiper.com.cn/api/index.html)
+
+# 已完成页面
+
+- 首页 `index.html`
+- 关于我们 (单页/内容页) `about.html`
+- 联系我们 `contact.html`
+
+# 更新记录
+
+## 2016-9-7
+
+> - 初版制作
---
+++ js/scripts.js
@@ -0,0 +1,116 @@
+// fix:active
+document.body.addEventListener('touchstart', function() {});
+
+// swiper slideshow
+var swiperSlideshow = new Swiper('#swiperSlideshow', {
+    pagination: '.swiper-pagination',
+    autoHeight: true,
+    autoplay: 5000,
+    loop: true
+});
+
+// swiper tab
+var swiperTab = new Swiper('#swiperTab', {
+    autoHeight: true,
+    onSlideChangeStart: function() {
+        $(".swiperTab .active").removeClass('active')
+        $(".swiperTab a").eq(swiperTab.activeIndex).addClass('active')
+    }
+});
+
+// switchery
+var elems = Array.prototype.slice.call(document.querySelectorAll('.iSwitchery'));
+elems.forEach(function(html) {
+    var switchery = new Switchery(html, {
+        className: 'iSwitchery',
+        color: '#fac800'
+    });
+});
+
+// jQuery plugin
+jQuery(document).ready(function($) {
+
+    // a.active
+    $("a").click(function() {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+    // menu
+    $("#menu").mmenu({
+        'navbar': {
+            "add": false
+        },
+        "extensions": [
+            "pagedim-black"
+        ],
+        "offCanvas": {
+            "position": "right"
+        }
+    });
+
+    // fancy box
+    $(".fancyBox").fancybox({
+        padding: 0,
+        margin: 25,
+        minWidth: 300,
+        maxWidth: 1000,
+        closeBtn: false,
+        helpers: {
+            overlay: {
+                closeClick: false
+            }
+        }
+    });
+
+    $(".fancyPage").fancybox({
+        type: 'inline',
+        padding: 0,
+        margin: 0,
+        height: 335,
+        closeBtn: false,
+        autoSize: false,
+        wrapCSS: 'fancyPageContent',
+        topRatio: 0,
+        helpers: {
+            overlay: {
+                closeClick: false
+            }
+        }
+    });
+
+    // swiper tab
+    $(".swiperTab a").on('touchstart mousedown', function(e) {
+        e.preventDefault()
+        $(".swiperTab .active").removeClass('active')
+        $(this).addClass('active')
+        swiperTab.slideTo($(this).index())
+    });
+    $(".swiperTab a").click(function(e) {
+        e.preventDefault()
+    });
+
+    // iCheck
+    $('.iCheckInput').iCheck({
+        checkboxClass: 'iCheck',
+        radioClass: 'iRadio',
+    });
+    // 判断选中状态
+    // $("#submit").click(function() {
+    //
+    //     直接获取选中radio的值
+    //     alert($("input[name='radio-1']:checked").val());
+    //
+    //     循环判断是否选中
+    //     $("input[name='radio-1']:radio").each(function() {
+    //         if (true == $(this).is(':checked')) {
+    //             alert($(this).val());
+    //         }
+    //     });
+    //
+    // });
+
+    $(".selection .group").click(function() {
+        $(this).addClass("active").siblings().removeClass("active");
+    });
+
+});
